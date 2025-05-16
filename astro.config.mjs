2fd19e694cbd36ae58bf0a4f3c18441d187dec63
Ali Madooei <alimadooei@gmail.com>
2025-05-16 00:30:43 -0400

    Update Astro configuration to enable prefetching and set output to static, optimizing performance and build settings.

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  prefetch: true,
+  output: "static",
   integrations: [react()],
-
   vite: {
     plugins: [tailwindcss()],
   },
