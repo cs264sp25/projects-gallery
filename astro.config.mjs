d5363da97c80cb7beb89faa0c89552731175610e
Ali Madooei <alimadooei@gmail.com>
2025-05-16 06:24:07 -0400

    Add Netlify integration and configuration for deployment, including .netlify folder in .gitignore and netlify.toml file for build settings. Update package.json to include @astrojs/netlify dependency.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,17 @@ import react from "@astrojs/react";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
   output: "static",
   integrations: [react()],
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: netlify(),
 });
